f7eb902d28ce9eea2e000cb93e6ba454c8aff67a
Oleg Truskov <125879683+OlegTruskov@users.noreply.github.com>
2024-05-12 18:20:06 +0800

    S41.cpp

---
+++ S41.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int squared_sum(double a, double b)
+{
+    double c;
+    c = (a + b)*(a+b);
+    return c;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double a, b;
+cout << "Введите первое число: ";
+cin >> a;
+cout << "Введите второе число: ";
+cin >> b;
+cout << "Квадрат суммы чисел равен: " << squared_sum(a, b);
+return 0;
+}
