9c63d918f562bd6087b6415f3f5d4db45e5b2007
Oleg Truskov <125879683+OlegTruskov@users.noreply.github.com>
2024-05-13 12:51:30 +0800

    S02.cpp

--- S02.cpp
+++ S02.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
-       int a = 13, b = 3; // Задаем переменные
+      int a = 13, b = 3; // Задаем переменные
     cout << "a = " << a << endl << "b = " << b << endl;
             int c = a / b; // Делим нацело
             int d = a % b; // Находим остаток от деления
